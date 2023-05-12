5769e5926f2391c7c6e5003e2ec9a501467e7247
Jean F. dos Santos <jeanddg@hotmail.com>
2023-05-12 13:54:35 -0300

    test: refactor product service test

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,8 +1,7 @@
 import { ProductService } from './product.service';
 import { CreateProductDto } from './types/dtos/productDto';
 import { randomUUID } from 'crypto';
-import type { ProductRepository } from './product.repository';
-import { MovementRepository } from 'src/movement/movement.repository';
+import type { AbstractProductRepository } from './product.repository';
 
 describe('ProductService', () => {
   beforeEach(async () => {
@@ -19,7 +18,7 @@ describe('ProductService', () => {
   productMock.minStock = 5;
   productMock.price = '10.50';
 
-  const productRepository: ProductRepository = {
+  const productRepositoryMock: AbstractProductRepository = {
     create: jest.fn(),
     update: jest.fn(),
     findAll: jest.fn(),
@@ -28,30 +27,18 @@ describe('ProductService', () => {
     subtractProduct: jest.fn(),
   };
 
-  const movementRepository: MovementRepository = {
-    create: jest.fn(),
-  };
-
   describe('#createProduct', () => {
     test('should create a product', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        create: jest.fn().mockResolvedValue({
-          id: randomUUID(),
-          ...productMock,
-        }),
+      productRepositoryMock.create = jest.fn().mockResolvedValue({
+        id: randomUUID(),
+        ...productMock,
       });
 
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       await service.create(productMock);
 
-      expect(repositoryMock.create).toHaveBeenCalled();
-      //expect(movementRepositoryMock.create).toHaveBeenCalled();
+      expect(productRepositoryMock.create).toHaveBeenCalled();
     });
   });
 
@@ -59,142 +46,93 @@ describe('ProductService', () => {
     test('should update a product', async () => {
       const ID = randomUUID();
 
-      const repositoryMock = Object.assign({}, productRepository, {
-        update: jest.fn().mockResolvedValue({ id: ID, ...productMock }),
-      });
+      productRepositoryMock.update = jest
+        .fn()
+        .mockResolvedValue({ id: ID, ...productMock });
 
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       await service.update(ID, productMock);
 
-      expect(repositoryMock.update).toHaveBeenCalled();
+      expect(productRepositoryMock.update).toHaveBeenCalled();
     });
   });
 
   describe('#findAllProducts paginated', () => {
     test('should find products', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        findAll: jest.fn().mockResolvedValue({
-          data: [],
-          count: 50,
-          limit: 10,
-        }),
+      productRepositoryMock.findAll = jest.fn().mockResolvedValue({
+        data: [],
+        count: 50,
+        limit: 10,
       });
 
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
       const result = await service.findAll('', 1);
 
       expect(result).toHaveProperty('data');
       expect(result).toHaveProperty('count');
       expect(result).toHaveProperty('limit');
 
-      expect(repositoryMock.findAll).toHaveBeenCalled();
+      expect(productRepositoryMock.findAll).toHaveBeenCalled();
     });
   });
 
   describe('#findByCode paginated', () => {
     test('should not find a product by its code if not exists', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        findAll: jest.fn().mockResolvedValue({
-          data: [],
-          count: 50,
-          limit: 10,
-        }),
-        findByCode: jest.fn().mockResolvedValue(undefined),
+      productRepositoryMock.findAll = jest.fn().mockResolvedValue({
+        data: [],
+        count: 50,
+        limit: 10,
       });
 
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       const result = await service.findByCode('123456');
 
       expect(result).toBeFalsy();
-      expect(repositoryMock.findByCode).toHaveBeenCalled();
+      expect(productRepositoryMock.findByCode).toHaveBeenCalled();
     });
 
     test('should find a product by its code if it exists', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        findAll: jest.fn().mockResolvedValue({
-          data: [],
-          count: 50,
-          limit: 10,
-        }),
-        findByCode: jest.fn().mockResolvedValue({}),
+      productRepositoryMock.findByCode = jest.fn().mockResolvedValue({});
+      productRepositoryMock.findAll = jest.fn().mockResolvedValue({
+        data: [],
+        count: 50,
+        limit: 10,
       });
 
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       const result = await service.findByCode('123456');
 
       expect(result).toBeTruthy();
-      expect(repositoryMock.findByCode).toHaveBeenCalled();
+      expect(productRepositoryMock.findByCode).toHaveBeenCalled();
     });
   });
 
   describe('#addProduct', () => {
     test('should add the passed quantity to the current quantity of the product', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        findAll: jest.fn(),
-        findByCode: jest.fn(),
-        addProduct: jest.fn(),
-      });
-
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       await service.addProduct({
         productId: 'some-id',
         quantity: 10,
       });
 
-      expect(repositoryMock.addProduct).toHaveBeenCalled();
+      expect(productRepositoryMock.addProduct).toHaveBeenCalled();
     });
   });
 
   describe('#subtractProduct', () => {
     test('should add the passed quantity to the current quantity of the product', async () => {
-      const repositoryMock = Object.assign({}, productRepository, {
-        findAll: jest.fn(),
-        findByCode: jest.fn(),
-        subtractProduct: jest.fn(),
-      });
-
-      const movementRepositoryMock = Object.assign({}, movementRepository);
-
-      const service: ProductService = new ProductService(
-        repositoryMock,
-        movementRepositoryMock,
-      );
+      const service: ProductService = new ProductService(productRepositoryMock);
 
       await service.subtractProduct({
         productId: 'some-id',
         quantity: 10,
       });
 
-      expect(repositoryMock.subtractProduct).toHaveBeenCalled();
+      expect(productRepositoryMock.subtractProduct).toHaveBeenCalled();
     });
   });
 });
