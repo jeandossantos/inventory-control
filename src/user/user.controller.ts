a57383c9ae2246c13d399bca956f58109576639b
Jean F. dos Santos <jeanddg@hotmail.com>
2023-05-12 14:25:42 -0300

    feat: add Put /users/:id endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './types/dtos/createUserDto';
+import { UpdateProductDto } from 'src/product/types/dtos/productDto';
+import { UpdateUserDto } from './types/dtos/updateUserDto';
 
 @Controller('users')
 export class UserController {
@@ -8,5 +17,13 @@ export class UserController {
   @Post()
   async create(@Body() dto: CreateUserDto) {
     await this.userService.create(dto);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    await this.userService.update(id, dto);
   }
 }
