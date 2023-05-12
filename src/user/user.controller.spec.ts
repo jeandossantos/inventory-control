335b2d0b12c78ded72290cf6119dda89c21a2240
Jean F. dos Santos <jeanddg@hotmail.com>
2023-05-12 14:45:35 -0300

    test: add test for service.update

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from '../app.module';
 import { CreateUserDto } from './types/dtos/createUserDto';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserData } from './types/user.interface';
+import { hashPassword } from '../utils/hashPassword';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -34,6 +36,24 @@ describe('UserController (e2e)', () => {
     confirmedPassword: 'password',
     isAdmin: false,
   };
+
+  async function createUser(user: CreateUserData) {
+    const { name, email, password, isAdmin } = user;
+    const prisma = new PrismaService();
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        isAdmin,
+        password: await hashPassword(password),
+      },
+    });
+
+    await prisma.$disconnect();
+
+    return newUser;
+  }
 
   describe('/users (POST)', () => {
     test('should return 400 if passwords does not match!', async () => {
@@ -70,6 +90,21 @@ describe('UserController (e2e)', () => {
 
       expect(response.statusCode).toBe(400);
       expect(response.body.message).toBe('User not found!');
+    });
+
+    test('should update a user!', async () => {
+      const { email, name } = userMock;
+
+      const { id } = await createUser({
+        ...userMock,
+        email: 'user-to-be-updated@example.com',
+      });
+
+      let response = await request(app.getHttpServer())
+        .put(`/users/${id}`)
+        .send({ email: 'updated-user@example.com', name: 'updated name' });
+
+      expect(response.statusCode).toBe(200);
     });
   });
 });
