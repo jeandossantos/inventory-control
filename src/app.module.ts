d364d413680bb402522aca65592208048d944934
Jean F. dos Santos <jeanddg@hotmail.com>
2023-05-13 13:05:52 -0300

    feat: apply middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { MovementModule } from './movement/movement.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { isAuthenticated } from './middleware/isAuthenticated';
+import { UserController } from './user/user.controller';
+import { MovementController } from './movement/movement.controller';
+import { ProductController } from './product/product.controller';
+import { isAdmin } from './middleware/isAdmin';
 
 @Module({
   imports: [
@@ -17,4 +22,12 @@ import { ProductModule } from './product/product.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(isAuthenticated)
+      .forRoutes(UserController, MovementController, ProductController)
+      .apply(isAdmin)
+      .forRoutes(UserController);
+  }
+}
