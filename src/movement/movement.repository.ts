9ee3398fa64ca4e6ac399a0d5ca3883b622ec6fc
Jean F. dos Santos <jeanddg@hotmail.com>
2023-05-14 17:48:06 -0300

    feat implement getAll

--- src/movement/movement.repository.ts
+++ src/movement/movement.repository.ts
@@ -3,14 +3,66 @@ import {
   PaginatedMovement,
   getMovementsData,
 } from './types/movement.interface';
+import { PrismaService } from '../prisma/prisma.service';
 
-interface IMovementRepository {
-  getAll(data: getMovementsData): Promise<PaginatedMovement>;
+export abstract class IMovementRepository {
+  abstract getAll(data: getMovementsData): Promise<PaginatedMovement>;
 }
 
 @Injectable()
-export class MovementRepository implements IMovementRepository {
-  getAll(data: getMovementsData): Promise<PaginatedMovement> {
-    throw new Error('Method not implemented.');
+export class MovementRepository extends IMovementRepository {
+  constructor(private readonly prisma: PrismaService) {
+    super();
+  }
+
+  async getAll({
+    from,
+    to,
+    page,
+    search,
+  }: getMovementsData): Promise<PaginatedMovement> {
+    const limit = 10;
+    const skip = (page - 1) * limit;
+    let where = {};
+
+    if (from && to) {
+      where = {
+        moment: {
+          gte: from,
+          lte: to,
+        },
+      };
+    }
+
+    const count = await this.prisma.movement.count({
+      where,
+    });
+
+    const movements = await this.prisma.movement.findMany({
+      where,
+      skip,
+      take: limit,
+      orderBy: { moment: 'desc' },
+      include: {
+        product: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    return {
+      count,
+      limit,
+      data: movements,
+    };
   }
 }
